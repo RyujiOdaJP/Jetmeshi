93fbd236b8c40d980191f5085ea53bb37d751ed2
Ryuji Oda <ryuji.oda@gmail.com>
2020-05-02 03:09:05 +0900

    name uniqed

--- docker-laravel/src/database/migrations/2020_05_01_163512_create_tags_table.php
+++ docker-laravel/src/database/migrations/2020_05_01_163512_create_tags_table.php
@@ -15,8 +15,8 @@ class CreateTagsTable extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id('tag_id');
-            $table->string('name');
-            $table->timestamps()->nullable();
+            $table->string('name')->unique();
+            $table->timestamps();
 
         });
     }
