443188363d51b6287a0b26d271f82e8a907f16a6
Ryuji Oda <ryuji.oda@gmail.com>
2020-05-03 12:01:56 +0900

    rename img's columns

--- docker-laravel/src/database/migrations/2020_05_03_023858_add_sumnail_to_posts_table.php
+++ docker-laravel/src/database/migrations/2020_05_03_023858_add_sumnail_to_posts_table.php
@@ -17,6 +17,11 @@ class AddSumnailToPostsTable extends Migration
             //Add sumnail columns
             $table->string('sumnail_pc')->after('image_top');
             $table->string('sumnail_mobile')->after('sumnail_pc');
+            $table->renameColumn('image_2','image_seq1');
+            $table->renameColumn('image_3','image_seq2');
+            $table->renameColumn('image_4','image_seq3');
+            $table->renameColumn('image_5','image_seq4');
+            $table->renameColumn('sequence','sequence_body');
         });
     }
 
@@ -31,6 +36,11 @@ class AddSumnailToPostsTable extends Migration
             //
             $table->dropColumn('sumnail_pc');
             $table->dropColumn('sumnail_mobile');
+            $table->renameColumn('image_seq1','image_2');
+            $table->renameColumn('image_seq2','image_3');
+            $table->renameColumn('image_seq3','image_4');
+            $table->renameColumn('image_seq4','image_5');
+            $table->renameColumn('sequence_body','sequence');
         });
     }
 }
