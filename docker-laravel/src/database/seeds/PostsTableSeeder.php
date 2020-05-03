deb35756e5bfac6d107f4c72eb57373005d550b5
Ryuji Oda <ryuji.oda@gmail.com>
2020-05-03 15:21:24 +0900

    insert initial data

--- docker-laravel/src/database/seeds/PostsTableSeeder.php
+++ docker-laravel/src/database/seeds/PostsTableSeeder.php
@@ -12,5 +12,30 @@ class PostsTableSeeder extends Seeder
     public function run()
     {
         //
+        $faker = Faker\Factory::create('ja_JP');
+
+        // ランダムに記事を作成
+        for ($i = 0; $i < 40; $i++) {
+            DB::table('posts')->insert([
+                'created_at' => $faker->dateTime(),
+                'updated_at' => $faker->dateTime(),
+                'title' => $faker->text(20),
+                'image_top' => $faker->url,
+                'sumnail_pc' => $faker->url,
+                'sumnail_mobile' => $faker->url,
+                'image_seq1' => $faker->url,
+                'image_seq2' => $faker->url,
+                'sequence_body' => $faker->text(200),
+                'cooking_time' => $faker->numberBetween(1, 40),
+                'budget' => $faker->numberBetween(100, 500),
+                'user_id' => $faker->numberBetween(1, 20)
+            ]);
+        }
+        // for ($i = 0; $i < 10; $i++) {
+        //     DB::table('posts')->insert([
+        //         'image_seq3' => $faker->url,
+        //         'image_seq4' => $faker->url
+        //     ]);
+        // }
     }
 }
--- docker-laravel/src/database/seeds/TagsTableSeeder.php
+++ docker-laravel/src/database/seeds/TagsTableSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Predis\Command\ListLength;
+
+use function GuzzleHttp\_current_time;
 
 class TagsTableSeeder extends Seeder
 {
@@ -11,6 +14,30 @@ class TagsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //clear data
+        // DB::table('posts')->truncate();
+
+        //insert tags
+        $tags=
+            [
+            '皿洗い不要',
+            'レンジのみ',
+            'お湯のみ',
+            '食物繊維豊富',
+            'ミネラル豊富',
+            'ビタミン豊富',
+            '低カロリー',
+            '高カロリー',
+            '長期保存可',
+            '高タンパク',
+            '低脂肪',
+            '低糖質'
+
+            ];
+
+        for ($i = 0; $i < count($tags); $i++)
+        DB::table('tags')->insert([
+            'name' => $tags[$i]
+        ]);
     }
 }
