37c6689fa32db4b6e0be12487c2f44e676124a1b
Ryuji Oda <ryuji.oda@gmail.com>
2020-05-03 19:59:21 +0900

    initial seeding

---
+++ docker-laravel/src/database/seeds/LikesTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class LikesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Use faker
+        $faker = Faker\Factory::create('ja_JP');
+
+        for ($i = 0; $i < 40; $i++) {
+            DB::table('likes')->insert([
+                'created_at' => $faker->dateTime(),
+                'updated_at' => $faker->dateTime(),
+
+                'user_id' => $faker->numberBetween(1, 20),
+                'post_id' => $faker->numberBetween(1, 80)
+            ]);
+        }
+    }
+}
