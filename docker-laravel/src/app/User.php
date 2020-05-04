6bc95a90681c1d8c07a992f4f10ee3d9ec9e9b35
Ryuji Oda <ryuji.oda@gmail.com>
2020-05-05 02:54:12 +0900

    added models

--- docker-laravel/src/app/User.php
+++ docker-laravel/src/app/User.php
@@ -6,6 +6,8 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+//モデルはテーブルとマッピングされたオブジェクトです。
+// DB操作を行うためのクラスになります。
 class User extends Authenticatable
 {
     use Notifiable;
