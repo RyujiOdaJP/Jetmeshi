d2f1ddd1e88d8e821a355d77c72a8f12f356b16f
Ryuji Oda <ryuji.oda@gmail.com>
2020-05-06 21:12:52 +0900

    set scroll.js

--- docker-laravel/src/resources/views/layouts/app.blade.php
+++ docker-laravel/src/resources/views/layouts/app.blade.php
@@ -11,7 +11,8 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-
+    <script src="{{ asset('jquery-3.5.1.min.js')}}"></script>
+    <script src="{{ asset('js/infinite-scroll.pkgd.min.js')}}"></script>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
