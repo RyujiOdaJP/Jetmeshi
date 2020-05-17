a71d04435e209bfbac26deaee63037225c8e606b
Ryuji Oda <ryuji.oda@gmail.com>
2020-05-18 05:20:54 +0900

    add blob

--- docker-laravel/src/resources/views/post/edit.blade.php
+++ docker-laravel/src/resources/views/post/edit.blade.php
@@ -29,12 +29,12 @@
             @endif
         </div>
         <div class="row form-group justify-content-around">
-            <div class="col2">
+            <div class="col-md-4">
                 <label for="image_top">{{ __('image_top') }}</label>
-                <input id="image_top" type="file" class="form-control
+                <input id="image_top" type="file" class="form-control image
                 @if ($errors->has('image_top')) is-invalid
-                @endif" name="image_top" rows="8" required>
-                    <img src="{{ old('image_top',$post->image_top) }}" alt="" width="560px"> 
+                @endif" name="top" rows="8" accept="image/bmp,image/gif,image/jpeg,image/png" required>
+                    <span id="target_image_top" class="target_image"><img src="{{ old('image_top',$post->image_top) }}" alt="" width="100%"></span>
                 {{-- Insert JS into src='' as tempolary URL--}}
                 </input>
                 @if ($errors->has('image_top'))
@@ -43,12 +43,12 @@
                     </span>
                 @endif
             </div>
-            <div class="col2">
+            <div class="col-md-4">
                 <label for="image_seq1">{{ __('image_seq1') }}</label>
-                <input id="image_seq1" type="file" class="form-control
+                <input id="image_seq1" type="file" class="form-control image
                 @if ($errors->has('image_seq1')) is-invalid
-                @endif" name="image_seq1" rows="8" required>
-                    {{ old('image_seq1') }}
+                @endif" name="seq1" rows="8" accept="image/bmp,image/gif,image/jpeg,image/png" required>
+                    <span id="target_image_seq1" class="target_image"><img src="{{ old('image_seq1',$post->image_seq1) }}" alt="" width="100%"></span>
                 </input>
                 @if ($errors->has('image_seq1'))
                     <span class="invalid-feedback" role="alert">
@@ -56,13 +56,52 @@
                     </span>
                 @endif
             </div>
+            <div class="col-md-4">
+                <label for="image_seq2">{{ __('image_seq2') }}</label>
+                <input id="image_seq2" type="file" class="form-control image
+                @if ($errors->has('image_seq2')) is-invalid
+                @endif" name="seq1" rows="8" accept="image/bmp,image/gif,image/jpeg,image/png" required>
+                    <span id="target_image_seq2" class="target_image"><img src="{{ old('image_seq2',$post->image_seq2) }}" alt="" width="100%"></span>
+                </input>
+                @if ($errors->has('image_seq2'))
+                    <span class="invalid-feedback" role="alert">
+                        {{ $errors->first('image_seq2') }}
+                    </span>
+                @endif
+            </div>
+            <div class="col-md-4">
+                <label for="image_seq3">{{ __('image_seq3') }}</label>
+                <input id="image_seq3" type="file" class="form-control image
+                @if ($errors->has('image_seq3')) is-invalid
+                @endif" name="seq1" rows="8" accept="image/bmp,image/gif,image/jpeg,image/png" required>
+                    <span id="target_image_seq3" class="target_image"><img src="{{ old('image_seq3',$post->image_seq3) }}" alt="" width="100%"></span>
+                </input>
+                @if ($errors->has('image_seq3'))
+                    <span class="invalid-feedback" role="alert">
+                        {{ $errors->first('image_seq3') }}
+                    </span>
+                @endif
+            </div>
+            <div class="col-md-4">
+                <label for="image_seq4">{{ __('image_seq4') }}</label>
+                <input id="image_seq4" type="file" class="form-control image
+                @if ($errors->has('image_seq4')) is-invalid
+                @endif" name="seq1" rows="8" accept="image/bmp,image/gif,image/jpeg,image/png" required>
+                    <span id="target_image_seq4" class="target_image"><img src="{{ old('image_seq4',$post->image_seq4) }}" alt="" width="100%"></span>
+                </input>
+                @if ($errors->has('image_seq4'))
+                    <span class="invalid-feedback" role="alert">
+                        {{ $errors->first('image_seq4') }}
+                    </span>
+                @endif
+            </div>
         </div>
 
             <div class="row form-group justify-content-around">
                 <div class="col2">
-                    <label for="cooking_time">{{ __('調理時間 ') }} <span id="target_cooking_time">10</span>分</label>
-                    <input id="cooking_time" type="range"  min="0" max="60" value="10" step="1" class="custom-range" name="cooking_time" rows="8" required>
-                        {{ old('cooking_time') }}
+                    <label for="cooking_time">{{ __('調理時間 ') }} <span id="target_cooking_time">{{ old('cooking_time', $post->cooking_time) }}</span>分</label>
+                    <input id="cooking_time" type="range"  min="0" max="60" value="{{ old('cooking_time', $post->cooking_time) }}" step="1" class="custom-range" name="cooking_time" rows="8" required>
+
 
                     </input>
 
@@ -73,8 +112,8 @@
                     @endif
                 </div>
                 <div class="col2">
-                    <label for="budget">{{ __('調理費用 ') }}<span id="target_budget">100</span>円</label>
-                    <input id="budget" type="range" min="0" max="2000" value="100" step="10" class="custom-range" name="budget" rows="8" required>
+                    <label for="budget">{{ __('調理費用 ') }}<span id="target_budget">{{ old('budget', $post->budget) }}</span>円</label>
+                    <input id="budget" type="range" min="0" max="2000" value="{{ old('budget', $post->budget) }}" step="10" class="custom-range" name="budget" rows="8" required>
                         {{ old('budget') }}
                     </input>
                     @if ($errors->has('budget'))
