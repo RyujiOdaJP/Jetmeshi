c39c6f183bcac66b6ab44bbe28337ca8b53ddeef
Ryuji Oda <ryuji.oda@gmail.com>
2020-05-19 05:53:06 +0900

    fix drawer with jQuery

--- docker-laravel/src/resources/views/layouts/app.blade.php
+++ docker-laravel/src/resources/views/layouts/app.blade.php
@@ -12,9 +12,15 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     {{-- <script src="{{ asset('js/bootstrap.js') }}" defer></script> --}}
-    {{-- <script src="{{ asset('js/js/jquery-3.5.1.min.js')}}"></script> --}}
+    {{-- <script src="{{ asset('js/jquery-3.5.1.min.js')}}"></script> --}}
+    <script src="{{ asset('js/myscript.js')}}"></script>
+    <script
+  src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+  integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs="
+  crossorigin="anonymous"></script>
     <script src="{{ asset('js/infinite-scroll.pkgd.min.js')}}"></script>
     {{-- <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script> --}}
+    {{-- <script src="//code.jquery.com/mobile/1.5.0-alpha.1/jquery.mobile-1.5.0-alpha.1.min.js"></script> --}}
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
@@ -33,16 +39,16 @@
                     {{-- {{ config('app.name', 'Laravel') }} --}}
                     <img src={{ asset('images/logo3.png') }}  height="35" alt="">
                 </a>
-                <button type="button" class="navbar-toggler offcanvas-toggle mr-1"
-                data-toggle="offcanvas" data-target="#js-bootstrap-offcanvas"
+                <button id="nav-button" type="button" class="navbar-toggler mr-1 not-clicked"
+                data-toggle="button" data-target="#js-bootstrap-offcanvas"
                 aria-controls="js-bootstrap-offcanvas"
                 aria-expanded="false"
                 aria-label="{{ __('Toggle navigation') }}">
                         <span class="sr-only">Toggle navigation</span>
                         <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="navbar-collapse navbar-offcanvas
-                navbar-offcanvas-touch navbar-offcanvas-right navbar-light bg-white"
+                <div class="navbar-light bg-white navbar-collapse navbar-offcanvas
+                navbar-offcanvas-right out"
                 role="navigation" id="js-bootstrap-offcanvas">
                     <!-- Left Side Of Navbar -->
                     <ul id="nav-for-mobile-top" class="navbar-nav mr-auto">
@@ -115,5 +121,56 @@
         </footer>
     </div>
 
+<script type="text/javascript">
+jQuery(function swipeMenu() {
+	$("main").bind("touchstart", onTouchStart);
+	$("main").bind("touchmove", onTouchMove);
+    $("main").bind("touchend", onTouchEnd);
+
+
+    $("#nav-button").on("click", function(){
+      if ($("#nav-button").hasClass('not-clicked')) {
+        $("#js-bootstrap-offcanvas").removeClass('out').addClass('in');
+        $("#nav-button").removeClass('not-clicked').addClass('clicked');
+        return;
+      }
+      if ($("#nav-button").hasClass('clicked')) {
+        $("#js-bootstrap-offcanvas").removeClass('in').addClass('out');
+        $("#nav-button").removeClass('clicked').addClass('not-clicked');
+      }
+    });
+
+	let status = [], position;
+
+	//スワイプ開始時の横方向の座標を格納
+	function onTouchStart(event) {
+	    startPosition = getPosition(event);
+	}
+
+	//スワイプの方向を取得(数値でトリガー感度を調節)
+	function onTouchMove(event) {
+        if(startPosition < getPosition(event) + 15){
+            status = ['in', 'out', 'clicked', 'not-clicked',];
+        }else{
+            status = ['out', 'in', 'not-clicked', 'clicked',];
+        }
+
+	}
+
+	//スワイプ終了時にstatusをクラス名に指定
+	function onTouchEnd(event) {
+        $("#js-bootstrap-offcanvas").removeClass(status[0]).addClass(status[1]);
+        $('#nav-button').removeClass(status[2]).addClass(status[3]);
+	}
+
+	//横方向の座標を取得
+	function getPosition(event) {
+        console.log('swipingNow');
+        let position = event.originalEvent.touches[0].pageX
+        console.log(-position);
+		return position;
+	}
+});
+</script>
 </body>
 </html>
--- docker-laravel/src/webpack.mix.js
+++ docker-laravel/src/webpack.mix.js
@@ -12,4 +12,9 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    // .js('resources/js/myscript.js', 'public/js')
+    // .js('resources/js/bootstrap.js', 'public/js')
+    // .js('resources/js/jquery-3.5.1.min.js', 'public/js')
+    // .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/bootstrap-Offcanvas.scss', 'public/css')
+    ;
