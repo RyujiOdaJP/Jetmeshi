68bfa934fb66bbe1aa0377fd29246665842dc869
Ryuji Oda <ryuji.oda@gmail.com>
2020-05-22 07:19:24 +0900

    change variables

--- docker-laravel/src/public/js/range.js
+++ docker-laravel/src/public/js/range.js
@@ -5,11 +5,11 @@
   var targetBudget = document.getElementById('target_budget');
   var targetCookingTime = document.getElementById('target_cooking_time');
 
-  var rtnSliderValue = function (value, target) {
+  var getSliderValue = function (value, target) {
     return function () {
       target.innerHTML = value.value;
     };
   };
-  valueBudget.addEventListener('input', rtnSliderValue(valueBudget, targetBudget));
-  valueCookingTime.addEventListener('input', rtnSliderValue(valueCookingTime, targetCookingTime));
+  valueBudget.addEventListener('input', getSliderValue(valueBudget, targetBudget));
+  valueCookingTime.addEventListener('input', getSliderValue(valueCookingTime, targetCookingTime));
 })();
