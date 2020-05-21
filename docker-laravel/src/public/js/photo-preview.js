be0249c0531c9e97e11c39118d0f4836188500b6
Ryuji Oda <ryuji.oda@gmail.com>
2020-05-22 04:12:10 +0900

    add croppie

--- docker-laravel/src/public/js/photo-preview.js
+++ docker-laravel/src/public/js/photo-preview.js
@@ -1,70 +1,79 @@
-// showing preview of pics
 (function () {
-  document.getElementById('image_top').addEventListener('change', function () {
-    // フォームで選択された全ファイルを取得
-    var fileList = this.files;
-    var targetImageTop = document.getElementById('target_image_top');
-    // 個数分の画像を表示する
-    for (var i = 0, l = fileList.length; l > i; i++) {
+    let blobUrl;
+    function createBlob(fileList, target=targetImage){
+        for (let i = 0, l = fileList.length; l > i; i++) {
       // Blob URLの作成
-      var blobUrl = window.URL.createObjectURL(fileList[i]);
+      blobUrl = window.URL.createObjectURL(fileList[i]);
 
       // HTMLに書き出し (src属性にblob URLを指定)
-      targetImageTop.innerHTML = '<img src="' + blobUrl + '" width="100%">';
+      target.innerHTML = '<img src="' + blobUrl + '" width="100%">';
+      return blobUrl;
+      }
     }
+  document.getElementById('image_top').addEventListener('change', function () {
+    let fileList = this.files;
+    let targetImage = document.getElementById('target_image_top');
+    createBlob(fileList, targetImage);
+    console.log(blobUrl);
+
+    $uploadCrop = $('#croppie_image_top').croppie({
+        // enableExif: true,
+        viewport: {
+            width: 300,
+            height: 200,
+            type: 'square'
+        },
+        boundary: {
+            width: 300,
+            height: 300
+          },
+    });
+
+    $uploadCrop.croppie('bind',{
+        url: blobUrl,
+        points: [77,469,280,739]
+    });
+
+    $('.upload-result').on('click', function (ev) {
+        $uploadCrop.croppie('result', {
+            type: 'blob',
+            size: 'viewport'
+        }).then( function(blob){
+            createBlob(blob, targetImage);
+        } )
+    });
   });
 
   document.getElementById('image_seq1').addEventListener('change', function () {
     // フォームで選択された全ファイルを取得
-    var fileList = this.files;
-    var targetImageTop = document.getElementById('target_image_seq1');
+    let fileList = this.files;
+    let targetImage = document.getElementById('target_image_seq1');
     // 個数分の画像を表示する
-    for (var i = 0, l = fileList.length; l > i; i++) {
-      // Blob URLの作成
-      var blobUrl = window.URL.createObjectURL(fileList[i]);
-
-      // HTMLに書き出し (src属性にblob URLを指定)
-      targetImageTop.innerHTML = '<img src="' + blobUrl + '" width="100%">';
-    }
+    createBlob(fileList, targetImage);
   });
 
   document.getElementById('image_seq2').addEventListener('change', function () {
     // フォームで選択された全ファイルを取得
-    var fileList = this.files;
-    var targetImageTop = document.getElementById('target_image_seq2');
+    let fileList = this.files;
+    let targetImage = document.getElementById('target_image_seq2');
     // 個数分の画像を表示する
-    for (var i = 0, l = fileList.length; l > i; i++) {
-      // Blob URLの作成
-      var blobUrl = window.URL.createObjectURL(fileList[i]);
-
-      // HTMLに書き出し (src属性にblob URLを指定)
-      targetImageTop.innerHTML = '<img src="' + blobUrl + '" width="100%">';
-    }
+    createBlob(fileList, targetImage);
   });
+
   document.getElementById('image_seq3').addEventListener('change', function () {
     // フォームで選択された全ファイルを取得
-    var fileList = this.files;
-    var targetImageTop = document.getElementById('target_image_seq3');
+    let fileList = this.files;
+    let targetImage = document.getElementById('target_image_seq3');
     // 個数分の画像を表示する
-    for (var i = 0, l = fileList.length; l > i; i++) {
-      // Blob URLの作成
-      var blobUrl = window.URL.createObjectURL(fileList[i]);
-
-      // HTMLに書き出し (src属性にblob URLを指定)
-      targetImageTop.innerHTML = '<img src="' + blobUrl + '" width="100%">';
-    }
+    createBlob(fileList, targetImage);
   });
+
   document.getElementById('image_seq4').addEventListener('change', function () {
     // フォームで選択された全ファイルを取得
-    var fileList = this.files;
-    var targetImageTop = document.getElementById('target_image_seq4');
+    let fileList = this.files;
+    let targetImage = document.getElementById('target_image_seq4');
     // 個数分の画像を表示する
-    for (var i = 0, l = fileList.length; l > i; i++) {
-      // Blob URLの作成
-      var blobUrl = window.URL.createObjectURL(fileList[i]);
-
-      // HTMLに書き出し (src属性にblob URLを指定)
-      targetImageTop.innerHTML = '<img src="' + blobUrl + '" width="100%">';
-    }
+    createBlob(fileList, targetImage);
   });
 })();
+
--- docker-laravel/src/resources/views/post/edit.blade.php
+++ docker-laravel/src/resources/views/post/edit.blade.php
@@ -28,39 +28,113 @@
                 </span>
             @endif
         </div>
-        <div class="row form-group justify-content-around">
-            <div class="col-md-4">
-                <label for="image_top">{{ __('image_top') }}</label>
-                <input id="image_top" type="file" class="form-control image
-                @if ($errors->has('image_top')) is-invalid
-                @endif" name="top" rows="8" accept="image/bmp,image/gif,image/jpeg,image/png" required>
-                    <span id="target_image_top" class="target_image"><img src="{{ old('image_top',$post->image_top) }}" alt="" class="previews"></span>
-                {{-- Insert JS into src='' as tempolary URL--}}
-                </input>
-                @if ($errors->has('image_top'))
-                    <span class="invalid-feedback" role="alert">
-                        {{ $errors->first('image_top') }}
-                    </span>
-                @endif
+        {{-- <div class="demo-wrap upload-demo">
+            <div class="container">
+                <div class="col-1-2">
+                    <div class="actions">
+                        <a class="btn file-btn">
+                            <span>Upload</span>
+                            <input type="file" id="upload" value="Choose a file" accept="image/*" />
+                        </a>
+                        <button class="upload-result">Result</button>
+                    </div>
+                </div>
+                <div class="col-1-2">
+                    <div class="upload-msg">
+                        Upload a file to start cropping
+                    </div>
+                    <div class="upload-demo-wrap">
+                        <div id="upload-demo"></div>
+                    </div>
+                </div>
+
+        </div>
+        </div> --}}
+        {{-- <div class="demo-wrap upload-demo">
+            <div class="container">
+            <div class="grid">
+                <div class="col-1-2">
+                    <div class="actions">
+                        <a class="btn file-btn">
+                            <span>Upload</span>
+                            <input type="file" id="upload" value="Choose a file" accept="image/*" />
+                        </a>
+                        <button class="upload-result">Result</button>
+                    </div>
+                </div>
+                <div class="col-1-2">
+                    <div class="upload-msg">
+                        Upload a file to start cropping
+                    </div>
+                    <div class="upload-demo-wrap">
+                        <div id="upload-demo" class="croppie-container">
+                            <div class="cr-boundary" aria-dropeffect="none">
+                                <img class="cr-image" alt="preview" aria-grabbed="false">
+                                <div class="cr-viewport cr-vp-circle" tabindex="0" style="width: 100px; height: 100px;"></div>
+                                <div class="cr-overlay"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="col-md-4">
-                <label for="image_seq1">{{ __('image_seq1') }}</label>
-                <input id="image_seq1" type="file" class="form-control image
-                @if ($errors->has('image_seq1')) is-invalid
-                @endif" name="seq1" rows="8" accept="image/bmp,image/gif,image/jpeg,image/png" required>
-                    <span id="target_image_seq1" class="target_image"><img src="{{ old('image_seq1',$post->image_seq1) }}" alt="" class="previews"></span>
-                </input>
+        </div>
+        </div> --}}
+        <div class="row form-group justify-content-around">
+            <div id="croppie_image_top" class="col-md-6">
+             {{-- <input id="image_top" type="file" class="form-control image
+              @if ($errors->has('image_top')) is-invalid
+              @endif" name="top" rows="8" accept="image/jpeg,image/png" required></input> --}}
+              <label for="cr-boundary">{{ __('image_top') }}</label>
+              {{-- <div class="cr-boundary"> --}}
+
+               <span id="target_image_top" class="target_image">
+                  <img src="{{ old('image_top',$post->image_top) }}" alt="" class="position-static cr-image previews">
+               </span>
+               {{-- <div class="cr-viewport"></div>
+               <div class="cr-overlay"></div> --}}
+              {{-- </div> --}}
+             @if ($errors->has('image_top'))
+                <span class="invalid-feedback" role="alert">
+                    {{ $errors->first('image_top') }}
+                </span>
+             @endif
+                {{-- <div class="cr-slider-wrap">
+                  <input class="cr-slider" type="range" step="0.0001" aria-label="zoom" min="0.2667" max="1.5000" aria-valuenow="0.5552">
+                </div> --}}
+                <div class="actions">
+                    <a class="btn btn-info position-relative">
+                        <span>Upload</span>
+                        {{-- <input type="file" id="upload" value="Choose a file" accept="image/*" /> --}}
+                        <input id="image_top" type="file" class="form-control position-absolute
+                        @if ($errors->has('image_top')) is-invalid
+                        @endif" name="top" rows="8" accept="image/jpeg,image/png" style="opacity: 0;" required></input>
+                    </a>
+                    <a class="btn btn-outline-success upload-result" type="menu">Result</a>
+                  </div>
+            </div>
+
+            <div class="col-md-6">
+            　<label for="image_seq1">{{ __('image_seq1') }}</label>
+            　<input id="image_seq1" type="file" class="form-control" name="seq1" rows="8" accept="image/bmp,image/gif,image/jpeg,image/png" >
+            　</input>
+
+                <span id="target_image_seq1" class="target_image">
+                    <img src="{{ old('image_seq1',$post->image_seq1) }}" alt="" class="previews">
+                </span>
+                <div class="cr-slider-wrap"><input class="cr-slider" type="range" step="0.0001" aria-label="zoom" min="0.2667" max="1.5000" aria-valuenow="0.5552"></div>
                 @if ($errors->has('image_seq1'))
                     <span class="invalid-feedback" role="alert">
                         {{ $errors->first('image_seq1') }}
                     </span>
                 @endif
             </div>
-            <div class="col-md-4">
+
+
+            <div class="col-md-6">
                 <label for="image_seq2">{{ __('image_seq2') }}</label>
-                <input id="image_seq2" type="file" class="form-control image
+                <input id="image_seq2" type="file" class="form-control
                 @if ($errors->has('image_seq2')) is-invalid
-                @endif" name="seq1" rows="8" accept="image/bmp,image/gif,image/jpeg,image/png" required>
+                @endif" name="seq1" rows="8" accept="image/bmp,image/gif,image/jpeg,image/png">
                     <span id="target_image_seq2" class="target_image"><img src="{{ old('image_seq2',$post->image_seq2) }}" alt="" class="previews"></span>
                 </input>
                 @if ($errors->has('image_seq2'))
@@ -69,11 +143,12 @@
                     </span>
                 @endif
             </div>
-            <div class="col-md-4">
+
+            <div class="col-md-6">
                 <label for="image_seq3">{{ __('image_seq3') }}</label>
-                <input id="image_seq3" type="file" class="form-control image
+                <input id="image_seq3" type="file" class="form-control
                 @if ($errors->has('image_seq3')) is-invalid
-                @endif" name="seq1" rows="8" accept="image/bmp,image/gif,image/jpeg,image/png" required>
+                @endif" name="seq1" rows="8" accept="image/bmp,image/gif,image/jpeg,image/png">
                     <span id="target_image_seq3" class="target_image"><img src="{{ old('image_seq3',$post->image_seq3) }}" alt="" class="previews"></span>
                 </input>
                 @if ($errors->has('image_seq3'))
@@ -82,11 +157,11 @@
                     </span>
                 @endif
             </div>
-            <div class="col-md-4">
+            <div class="col-md-6">
                 <label for="image_seq4">{{ __('image_seq4') }}</label>
-                <input id="image_seq4" type="file" class="form-control image
+                <input id="image_seq4" type="file" class="form-control
                 @if ($errors->has('image_seq4')) is-invalid
-                @endif" name="seq1" rows="8" accept="image/bmp,image/gif,image/jpeg,image/png" required>
+                @endif" name="seq1" rows="8" accept="image/bmp,image/gif,image/jpeg,image/png">
                     <span id="target_image_seq4" class="target_image"><img src="{{ old('image_seq4',$post->image_seq4) }}" alt="" class="previews"></span>
                 </input>
                 @if ($errors->has('image_seq4'))
