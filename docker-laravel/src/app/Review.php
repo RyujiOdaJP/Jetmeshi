a073264092a66f5dbc4621ff54fc37ede23e3d17
Ryuji Oda <ryuji.oda@gmail.com>
2020-05-30 18:04:48 +0900

    add relation to User&Post

--- docker-laravel/src/app/Review.php
+++ docker-laravel/src/app/Review.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    //TODO: make relation with post&user
+    public function user() // 単数形
+    {
+        return $this->belongsTo('App\User');
+        // ->latest();
+    }
+    public function post() // 単数形
+    {
+        return $this->belongsTo('App\Post');
+        // ->latest();
+    }
 }
