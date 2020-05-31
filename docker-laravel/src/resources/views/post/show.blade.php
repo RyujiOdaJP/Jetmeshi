488d65dd78a54f0143c4c60c3f156b0e68946b83
Ryuji Oda <ryuji.oda@gmail.com>
2020-06-01 00:18:40 +0900

    rm comment

--- docker-laravel/src/resources/views/post/show.blade.php
+++ docker-laravel/src/resources/views/post/show.blade.php
@@ -4,34 +4,6 @@
 @extends('layouts.app')
 @section('content')
 
-{{-- <script>
-    var infScroll = new InfiniteScroll( '.scroll_area', {
-      path : ".pagination a[rel=next]",
-      append : ".post"
-    });
-</script> --}}
-{{-- “.pagination a[rel=next]” はLaravelの
-    paginateが出力するリンクの形に合わせています --}}
-{{--
-
-    Reference:https://mrkmyki.com/laravel%E3%81%AEpaginate%E3%81%A8infinite-scroll%E3%82%92%E4%BD%BF%E3%81%84%E3%80%81%E3%83%87%E3%83%BC%E3%82%BF%E3%83%99%E3%83%BC%E3%82%B9%E5%86%85%E3%81%AE%E3%83%87%E3%83%BC%E3%82%BF%E3%82%92
-
-    <section class="scroll_area"
-  data-infinite-scroll='{
-    "path": ".pagination a[rel=next]",
-    "append": ".post"
-  }'
->
-  @foreach($posts as $key => $post)
-    <div class="post">
-      <h3>{{ $post->title }}</h3>
-      <p>{{ $post->text }}</p>
-    </div>
-  @endforeach
-</section>
-{{ $posts->links() }} --}}
-
-
 <div class="container">
     <h1 id="post-title">{{ $title }}</h1>
 @auth
@@ -147,7 +119,6 @@
               <form action="{{ url('post/review/'.$post->id) }}" method="post" class="" enctype="multipart/form-data">
                 @csrf
                 @method('POST')
-                {{-- <input id="post_id" type="hidden" name="post_id" value="{{$id}}"> --}}
                 <div class="form-group row mb-2">
                   <div class="col-md-4">
                     <label for="stars" class="mb-0">評価</label>
