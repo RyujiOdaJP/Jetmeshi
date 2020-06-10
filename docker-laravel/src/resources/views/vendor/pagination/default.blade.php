ff14cc371bb652980992cf0ccbffa316824fc103
Ryuji Oda <ryuji.oda@gmail.com>
2020-06-10 23:10:01 +0900

    :ambulance: add: direction word

--- docker-laravel/src/resources/views/vendor/pagination/default.blade.php
+++ docker-laravel/src/resources/views/vendor/pagination/default.blade.php
@@ -4,22 +4,22 @@
             {{-- Previous Page Link --}}
             @if ($paginator->onFirstPage())
                 <li class="page-item disabled" aria-disabled="true">
-                    <span class="page-link"><i class="fas fa-angle-double-left"></i></span>
+                    <span class="page-link"><i class="fas fa-angle-double-left"></i>　前</span>
                 </li>
             @else
                 <li class="page-item">
-                    <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev"><i class="fas fa-angle-double-right"></i></a>
+                    <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev"><i class="fas fa-angle-double-left"></i>　前</a>
                 </li>
             @endif
 
             {{-- Next Page Link --}}
             @if ($paginator->hasMorePages())
                 <li class="page-item">
-                    <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next"><i class="fas fa-angle-double-right"></i></a>
+                    <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next">次　<i class="fas fa-angle-double-right"></i></a>
                 </li>
             @else
                 <li class="page-item disabled" aria-disabled="true">
-                    <span class="page-link"><i class="fas fa-angle-double-right"></i></span>
+                    <span class="page-link">次　<i class="fas fa-angle-double-right"></i></span>
                 </li>
             @endif
         </ul>
