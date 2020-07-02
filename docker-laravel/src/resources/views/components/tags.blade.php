aa067fcede1205820762cfb07a14280c136fc94a
Ryuji Oda <ryuji.oda@gmail.com>
2020-07-02 22:43:22 +0900

    :bug: fix: arrangement

--- docker-laravel/src/resources/views/components/tags.blade.php
+++ docker-laravel/src/resources/views/components/tags.blade.php
@@ -15,20 +15,25 @@
 </label>
 <div id="tag_container" class="row">
     <div id="tag_group1" class="col-6 form-group justify-content-between">
-        @for ( $i = 0; $i < floor( count($tags) / 2 ); $i++ ) <div class="custom-control custom-checkbox">
+        @for ( $i = 0; $i < floor( count($tags) / 2 ); $i++ )
+        <div class="custom-control custom-checkbox">
             <input type="checkbox" id="{{ 'customCheck'.$i }}" class="custom-control-input" name="tags[]"
                 value="{{ $i+1 }}" @if (is_array($post_tags[$i]['id'] ?? '' ) && in_array("$i", $post_tags[$i]['id']
                 ?? '' , true)) checked @endif>
             <label class="custom-control-label" for="{{ 'customCheck'.$i }}">{{ $tags[$i]['name'] }}</label>
+        </div>
+        @endfor
     </div>
-    @endfor
+
+
+    <div id="tag_group2" class="col-5 form-group justify-content-between">
+        @for ( $l = count($tags) - floor(count($tags) / 2), $i = $l; $i < floor(count($tags)); $i++ )
+        <div class="custom-control custom-checkbox">
+            <input type="checkbox" id="{{ 'customCheck' . $i }}" class="custom-control-input" name="tags[]"
+                value="{{ $i }}">
+            <label class="custom-control-label" for="{{ 'customCheck'. $i }}">{{ $tags[$i]['name'] }}</label>
+        </div>
+        @endfor
+    </div>
 </div>
 
-<div id="tag_group2" class="col-5 form-group justify-content-between">
-    @for ( $l = count($tags) - floor(count($tags) / 2), $i = $l; $i < floor(count($tags)); $i++ ) <div
-        class="custom-control custom-checkbox">
-        <input type="checkbox" id="{{ 'customCheck' . $i }}" class="custom-control-input" name="tags[]"
-            value="{{ $i }}">
-        <label class="custom-control-label" for="{{ 'customCheck'. $i }}">{{ $tags[$i]['name'] }}</label>
-</div>
-@endfor
