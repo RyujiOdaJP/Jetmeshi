91b19048040b8232b878c465c3ef8e27dd4e117e
Ryuji Oda <ryuji.oda@gmail.com>
2020-07-11 00:17:32 +0900

    :sparkles: add: make https as asset

--- docker-laravel/src/app/Providers/AppServiceProvider.php
+++ docker-laravel/src/app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +25,9 @@ class AppServiceProvider extends ServiceProvider
     // 管理者のID番号を1とする
     // 参照: https://stackoverflow.com/questions/28356193/
     config(['admin_id' => 1]);
+    $URL = new URL;
+    if (request()->isSecure()) {
+        $URL->forceScheme('https');
+    }
   }
 }
