403260b1fef939360ca1917606661b1fff89487a
Ryuji Oda <ryuji.oda@gmail.com>
2020-07-20 22:45:16 +0900

    :recycle: add: single action controller

---
+++ docker-laravel/src/app/Http/Controllers/IndexPostController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use App\Review; //import the post model.
+use App\Tag;
+
+class IndexPostController extends PostController
+{
+    public function __invoke()
+  {
+    $stars_avg = [];
+    $tag_names = [];
+    $posts = Post::latest()->Paginate(10);
+    // to display seraching window
+    $tags = Tag::all();
+
+    for ($i = 0; $i < count($posts); $i++) {
+      $names_array = [];
+      $stars_avg[] = Review::where('post_id', '=', $posts[$i]->id)->avg('stars');
+      $tag_values = $posts[$i]->tags()->get();
+
+      foreach ($tag_values as $tag_value) {
+        $names_array[] = $tag_value->name;
+      }
+      $tag_names[] = $names_array;
+    }
+    return view('post.index', compact('posts', 'stars_avg', 'tags', 'tag_names'));
+  }
+}
