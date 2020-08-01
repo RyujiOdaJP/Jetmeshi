beeeb57ff9b93944fa80a2595df5be686f777825
Ryuji Oda <ryuji.oda@gmail.com>
2020-08-01 15:09:06 +0900

    :zap: change img path form AP tp S3

--- docker-laravel/src/resources/views/top.blade.php
+++ docker-laravel/src/resources/views/top.blade.php
@@ -9,14 +9,8 @@ $title = env('APP_NAME');
 <div class="justify-content-center jumbotron jumbotron-fluid jumbotron-hero" >
     {{-- style="position:relative; height:100vh; background: url('./img/sample_meshi.jpg') center center; background-size: cover;" --}}
     <div class="container h-100">
-
-
         <h1 class="display-1 text-light">Jet Meshi</h1>
-
-
         <p class="lead col-md-8 text-light">今すぐ食べたいを叶える</ｐ>
-
-
       <div class="row justify-content-around align-content-end h-75" style= "bottom: 0;">
         <div class="col-4">
           <a href="{{ url('/post/create') }}" id="post_button" class="btn btn-success rounded-circle"
